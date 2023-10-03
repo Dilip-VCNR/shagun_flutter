d73405ae3145b338f99f8d2cc526253f9c37c456
santhosh kumar <santhosh.kumar@conigitalgroup.com>
2023-10-03 13:29:02 +0530

    internet connection handling

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_no_internet_widget/flutter_no_internet_widget.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:shagun_mobile/dashboard/transactions/view/received_shagun_for_event_screen.dart';
 import 'package:shagun_mobile/splash/view/on_boarding_screen.dart';
@@ -46,39 +47,58 @@ class MyApp extends StatelessWidget {
       DeviceOrientation.portraitUp,
       DeviceOrientation.portraitDown,
     ]);
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Shagun - Send and receive shagun on events',
-      theme: ThemeData(
-        textTheme: GoogleFonts.nunitoTextTheme(Theme.of(context).textTheme),
-        primaryColor: AppColors.primaryColor,
-        useMaterial3: true,
-        scaffoldBackgroundColor: AppColors.scaffoldBackground,
+    return InternetWidget(
+      offline: const FullScreenWidget(
+        child: Scaffold(
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image(image: AssetImage('assets/images/wireless.png'),width: 200,),
+                SizedBox(height: 20,),
+                Text("No Internet",style: TextStyle(fontSize: 24,fontWeight: FontWeight.bold),),
+                Text("Please turn on your wifi or internet connection to keep using the app",textAlign: TextAlign.center,style: TextStyle(fontSize: 18,fontWeight: FontWeight.normal),)
+              ],
+            ),
+          ),
+        )
       ),
-      initialRoute: Routes.splashRoute,
-      routes: {
-        Routes.splashRoute: (context) => const SplashScreen(),
-        Routes.onBoarding: (context) => const OnBoardingScreen(),
-        Routes.loginRoute: (context) => const LoginScreen(),
-        Routes.otpScreenRoute: (context) => const OtpScreen(),
-        Routes.registerRoute: (context) => const RegisterScreen(),
-        Routes.dashboardRoute: (context) => const DashboardScreen(),
-        Routes.webViewRoute: (context) => const WebViewScreen(),
-        Routes.notificationsRoute: (context) => const NotificationsScreen(),
-        Routes.allInvitationsRoute: (context) => const AllInvitations(),
-        Routes.eventDetailsRoute: (context) => const EventDetails(),
-        Routes.selectGreetingCardRoute: (context) => const SelectGreetingCard(),
-        Routes.greetingCardDetailRoute: (context) => const GreetingCardDetail(),
-        Routes.wishInputRoute: (context) => const WishInputScreen(),
-        Routes.checkOutRoute: (context) => const CheckOutScreen(),
-        Routes.profileSearchRoute: (context) => const ProfileSearchScreen(),
-        Routes.userEventsRoute: (context) => const UserEventsScreen(),
-        Routes.qrScannerRoute: (context) => const QrScannerScreen(),
-        Routes.giftsForEventRoute: (context) => const ReceivedShagunForEventScreen(),
-        Routes.orderSuccessRoute: (context) => const OrderSuccessScreen(),
-        Routes.trackOrderRoute: (context) => const TrackOrderScreen(),
-        Routes.searchTransactionsRoute: (context) => const SearchTransactionsScreen(),
-      },
+      loadingWidget: const Center(child: Text('Loading')),
+      online: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Shagun - Send and receive shagun on events',
+        theme: ThemeData(
+          textTheme: GoogleFonts.nunitoTextTheme(Theme.of(context).textTheme),
+          primaryColor: AppColors.primaryColor,
+          useMaterial3: true,
+          scaffoldBackgroundColor: AppColors.scaffoldBackground,
+        ),
+        initialRoute: Routes.splashRoute,
+        routes: {
+          Routes.splashRoute: (context) => const SplashScreen(),
+          Routes.onBoarding: (context) => const OnBoardingScreen(),
+          Routes.loginRoute: (context) => const LoginScreen(),
+          Routes.otpScreenRoute: (context) => const OtpScreen(),
+          Routes.registerRoute: (context) => const RegisterScreen(),
+          Routes.dashboardRoute: (context) => const DashboardScreen(),
+          Routes.webViewRoute: (context) => const WebViewScreen(),
+          Routes.notificationsRoute: (context) => const NotificationsScreen(),
+          Routes.allInvitationsRoute: (context) => const AllInvitations(),
+          Routes.eventDetailsRoute: (context) => const EventDetails(),
+          Routes.selectGreetingCardRoute: (context) => const SelectGreetingCard(),
+          Routes.greetingCardDetailRoute: (context) => const GreetingCardDetail(),
+          Routes.wishInputRoute: (context) => const WishInputScreen(),
+          Routes.checkOutRoute: (context) => const CheckOutScreen(),
+          Routes.profileSearchRoute: (context) => const ProfileSearchScreen(),
+          Routes.userEventsRoute: (context) => const UserEventsScreen(),
+          Routes.qrScannerRoute: (context) => const QrScannerScreen(),
+          Routes.giftsForEventRoute: (context) => const ReceivedShagunForEventScreen(),
+          Routes.orderSuccessRoute: (context) => const OrderSuccessScreen(),
+          Routes.trackOrderRoute: (context) => const TrackOrderScreen(),
+          Routes.searchTransactionsRoute: (context) => const SearchTransactionsScreen(),
+        },
+      ),
     );
   }
 }
